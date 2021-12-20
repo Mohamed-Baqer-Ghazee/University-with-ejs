e2caf7d12e3ddcdab9c5ced8856fd85a94e82388
FUB32 <the.crown1932@gmail.com>
2021-12-20 11:59:01 +0300

    added linked and changed fb to img

--- app.js
+++ app.js
@@ -1,139 +1,207 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-
 const app = express();
-
+const darkmode = require("darkmode-js");
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(__dirname + "/public"));
 app.use('/public/icons/', express.static('./public/icons'));
 
 app.locals._ = _;
 mongoose.connect("mongodb://localhost:27017/coursesDB");
 
-const courseSchema={
-  name:String,
-  primaryUrl:String,
-  secondaryUrl:[],
-  referencesUrl:[],
-  referencesImgs:[]
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#000', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
 }
-const Course = mongoose.model("course",courseSchema);
+const dark = new darkmode(options);
+dark.showWidget();
 
-app.get("/", function(req,res){
-  // Course.find({}).project({name:1}).toArray(function(err, result){
-  //   console.log(result);
-  // });
-    Course.find({}, function(err, foundCourses) {if (!err){
-    if (!foundCourses){
-      //Create a new list
-      res.render("notfound", {courseTitle: "fucku"});
-    } else {
-      //Show an existing list
-      const mnames=foundCourses.map(course => course.name);
-      console.log(mnames);
-      res.render("home", {
-        names:mnames
-      });
+const courseSchema = {
+  stage:String,
+  name: String,
+  primaryUrl: String,
+  secondaryUrl: [],
+  referencesUrl: [],
+  referencesImgs: []
+};
 
-    }
-  }else{
-    console.log("err");
-  }});
+const Course = mongoose.model("course", courseSchema);
+
+app.get("/", function(req, res) {
+  getNames();
+        res.render("home", {
+          names: names
+        });
 
 });
 
-app.get("/courses/:customCourseName", function(req, res){
-    const customCourseName =(req.params.customCourseName);
+app.get("/courses/:customCourseName", function(req, res) {
+  const customCourseName = (req.params.customCourseName);
 
-    Course.findOne({name: customCourseName}, function(err, foundCourse){
-      if (!err){
-        if (!foundCourse){
-          //Create a new list
-          res.render("notfound", {courseTitle: _.startCase(customCourseName)});
-        } else {
-          //Show an existing list
-          res.render("course", {
-            name: _.startCase(foundCourse.name),
-            primaryUrl: foundCourse.primaryUrl,
-            referencesUrl:foundCourse.referencesUrl,
-            referencesImgs:foundCourse.referencesImgs
-          });
-        }
-      }else{
-        console.log("err");
-      }
-    });
-
-});
-
-app.get("/create/main", function(req, res){
-  res.render("create");
-});
-
-app.post("/create/main", function(req, res){
-  const input=req.body;
-    const course = new Course({
-      name: input.name,
-      primaryUrl: input.primary,
-      secondaryUrl: input.secondary,
-      referencesUrl: input.references,
-      secondaryImgs: input.secondaryimg,
-      referencesImgs: input.referencesimg
-    });
-    course.save();
-      res.render("success",{courseTitle: _.startCase(input.name)});
-});
-
-
-
-app.get("/create/:customCourseName", function(req, res){
-
-  const customCourseName =(req.params.customCourseName);
-  Course.findOne({name: customCourseName}, function(err, foundCourse){
-    if (!err){
-      if (!foundCourse){
+  Course.findOne({
+    name: customCourseName
+  }, function(err, foundCourse) {
+    if (!err) {
+      if (!foundCourse) {
         //Create a new list
-        res.render("notfound", {courseTitle: _.startCase(customCourseName)});
+        res.render("notfound", {
+          names:names,
+          courseTitle: _.startCase(customCourseName)
+        });
       } else {
         //Show an existing list
-        res.render("add", {
-          deceratedTitle:_.startCase(foundCourse.name),
-          courseTitle: foundCourse.name,
+        res.render("course", {
+          names: names,
+          name: _.startCase(foundCourse.name),
           primaryUrl: foundCourse.primaryUrl,
           secondaryUrl: foundCourse.secondaryUrl,
-          referencesUrl:foundCourse.referencesUrl,
-          referencesImgs:foundCourse.referencesImgs
+          referencesUrl: foundCourse.referencesUrl,
+          referencesImgs: foundCourse.referencesImgs
         });
       }
-    }else{
+    } else {
       console.log("err");
     }
   });
+
+});
+
+app.get("/create/main", function(req, res) {
+
+        res.render("create", {
+          names: names
+        });
+      getNames();
+
+});
+
+app.post("/create/main", function(req, res) {
+  const input = req.body;
+  const course = new Course({
+    name: _.kebabCase(input.name),
+    primaryUrl: input.primary,
+    secondaryUrl: input.secondary,
+    referencesUrl: input.references,
+    secondaryImgs: input.secondaryimg,
+    referencesImgs: input.referencesimg
+  });
+  course.save();
+  res.render("success", {
+    names:names,
+    courseTitle: _.startCase(input.name)
+  });
+getNames();
 });
 
 
 
-app.post("/create/:courseTitle", function(req, res){
-  const courseTitle=req.params.courseTitle;
-  Course.updateOne(
-    {name: courseTitle},
-    {$set: req.body},
-    function(err){
-      if(!err){
-        res.render("success",{courseTitle: _.startCase(courseTitle)});
+app.get("/create/:customCourseName", function(req, res) {
+
+  const customCourseName = (req.params.customCourseName);
+  Course.findOne({
+    name: customCourseName
+  }, function(err, foundCourse) {
+    if (!err) {
+      if (!foundCourse) {
+        //Create a new list
+        res.render("notfound", {
+          names: names,
+          courseTitle: _.startCase(customCourseName)
+        });
+      } else {
+        //Show an existing list
+        res.render("add", {
+          names: names,
+          deceratedTitle: _.startCase(foundCourse.name),
+          courseTitle: foundCourse.name,
+          primaryUrl: foundCourse.primaryUrl,
+          secondaryUrl: foundCourse.secondaryUrl,
+          referencesUrl: foundCourse.referencesUrl,
+          referencesImgs: foundCourse.referencesImgs
+        });
+      }
+    } else {
+      console.log("err");
+    }
+  });
+getNames();
+});
+
+app.post("/create/:courseTitle", function(req, res) {
+  const courseTitle = req.params.courseTitle;
+  Course.updateOne({
+      name: courseTitle
+    }, {
+      $set: req.body
+    },
+    function(err) {
+      if (!err) {
+        res.render("success", {
+          names:names,
+          courseTitle: _.startCase(courseTitle)
+        });
       } else {
         res.send(err);
       }
     }
   );
+getNames();
 });
 
+app.get("/craete/:courseTitle/delete", function(req,res){
+  res.send("fuckkkkkkkkkkkk")
+})
+
+app.post("/create/:courseTitle/delete", function(req, res){
+  const nameToDelete= req.params.courseTitle;
+  console.log(nameToDelete);
+
+  Course.findOneAndDelete({name:nameToDelete}, function(err){
+    if (!err) {
+      console.log("Successfully deleted checked item.");
+      res.redirect("/");
+    }else {
+      console.log(err);
+    }
+  });
+getNames();
+});
+
+var names=[];
+function getNames(){
+  Course.find({}, function sub(err, foundCourses) {
+    if (!err) {
+      if (!foundCourses) {
+        res.render("notfound", {
+          names:names,
+          courseTitle: ""
+        });
+      } else {
+          names= foundCourses.map(course => course.name);
+      }
+    } else {
+      console.log("err");
+    }
+  });
+};
+
 app.listen(3000, function() {
+  getNames();
   console.log("Server started on port 3000");
 });
 
