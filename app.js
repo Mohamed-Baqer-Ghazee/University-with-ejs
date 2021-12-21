f67e879788ecef71f1a6efb045f425de84c63262
FUB32 <mohamedbaqer1932@gmail.com>
2021-12-22 01:22:03 +0300

    set heroku

--- app.js
+++ app.js
@@ -200,7 +200,12 @@ function getNames(){
   });
 };
 
-app.listen(3000, function() {
+let port=process.env.PORT;
+if(port==null || port==''){
+  port=3000;
+}
+
+app.listen(port, function() {
   getNames();
   console.log("Server started on port 3000");
 });
